d52aea7e8d91292c1005350d707395dc76f1cb03
leleliu008 <792793182@qq.com>
2016-04-04 18:26:53 +0800

    完善

--- change.sh
+++ change.sh
@@ -78,7 +78,7 @@ if [ $# = 2 ] ; then
         mv libs libs.bak2
 
         mv libs.bak libs
-        mv libs.bak2/* libs
+        mv -f libs.bak2/* libs
 	rm -rf libs.bak2
 else
 	echo 'please input new APP_NAME and PACKAGE_NAME'
